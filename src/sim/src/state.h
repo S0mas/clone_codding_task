acc24a7c3737b6093b26326cc943202a28383d17
krzysztof.sommerfeld <sommerfeld.krzysztof@gmail.com>
2024-04-08 17:44:58 +0200

    Add stateToString function

--- src/sim/src/state.h
+++ src/sim/src/state.h
@@ -2,9 +2,23 @@
 
 #include <half.hpp>
 
+#include <sstream>
+
 struct State
 {
     half_float::half pressure_{123.4};
     half_float::half temperature_{567.8};
     half_float::half velocity_{999.9};
 };
+
+inline auto stateToString(const State& state) -> std::string
+{
+    std::stringstream stream;
+    stream << '$';
+    stream << state.pressure_;
+    stream << ",";
+    stream << state.temperature_;
+    stream << ",";
+    stream << state.velocity_;
+    return stream.str();
+}
