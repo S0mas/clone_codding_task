75d70d5cf28a6025bce4eca501114fd9421c5f71
krzysztof.sommerfeld <sommerfeld.krzysztof@gmail.com>
2024-04-10 17:10:28 +0200

    Fix configure endpoint

--- src/http-server/include/server.cpp
+++ src/http-server/include/server.cpp
@@ -1,6 +1,54 @@
 #include "server.hpp"
 
 #include <QtHttpServer>
+#include <QJsonDocument>
+
+#include <optional>
+
+struct config
+{
+    int frequency = 0;
+    bool debug = false;
+};
+
+static std::optional<QJsonObject> byteArrayToJsonObject(const QByteArray &arr)
+{
+    QJsonParseError err;
+    const auto json = QJsonDocument::fromJson(arr, &err);
+    if (err.error || !json.isObject())
+        return std::nullopt;
+    return json.object();
+}
+
+std::optional<config> configFromJson(const QJsonObject &json)
+{
+    if(json.contains("frequency") && json.contains("debug"))
+    {
+        return config{json.value("frequency").toInt(), json.value("debug").toBool()};
+    }
+    else
+    {
+        return std::nullopt;
+    }
+}
+
+QHttpServerResponse addConfig(const QHttpServerRequest& request, DeviceController& controller)
+{
+    const std::optional<QJsonObject> json = byteArrayToJsonObject(request.body());
+    if(!json)
+    {
+        return QHttpServerResponse(QHttpServerResponder::StatusCode::BadRequest);
+    }
+
+    const auto& config = configFromJson(*json);
+    if(!config.has_value())
+    {
+        return QHttpServerResponse(QHttpServerResponder::StatusCode::BadRequest);
+    }
+
+    controller.configure(config.value().frequency, config.value().debug);
+    return QHttpServerResponse(QHttpServerResponder::StatusCode::Ok);
+}
 
 Server::Server(DeviceController& controller)
     : controller_{controller}
@@ -8,39 +56,38 @@ Server::Server(DeviceController& controller)
 {
 
     server_->route("/start", QHttpServerRequest::Method::Get,
-                     [&controller](const QHttpServerRequest &request) {
-                         controller.start();
-                         return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
-                     }
-                     );
+                   [&controller](const QHttpServerRequest &request) {
+                       controller.start();
+                       return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
+                   }
+                   );
 
     server_->route("/stop", QHttpServerRequest::Method::Get,
-                     [&controller](const QHttpServerRequest &request) {
-                         controller.stop();
-                         return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
-                     }
-                     );
+                   [&controller](const QHttpServerRequest &request) {
+                       controller.stop();
+                       return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
+                   }
+                   );
 
-    server_->route("/configure/<arg>/<arg>", QHttpServerRequest::Method::Get,
-                     [&controller](int frequency, int debug, const QHttpServerRequest &request) {
-                         controller.configure(frequency, static_cast<bool>(debug));
-                         return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
-                     }
-                     );
+    server_->route("/configure",
+                   [&controller](const QHttpServerRequest &request) {
+                       return addConfig(request, controller);
+                   }
+                   );
 
     server_->route("/messages/<arg>", QHttpServerRequest::Method::Get,
-                     [&controller](int numberOfMessagesToReturn, const QHttpServerRequest &request) {
-                         controller.start();
-                         return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
-                     }
-                     );
+                   [&controller](int numberOfMessagesToReturn, const QHttpServerRequest &request) {
+                       controller.start();
+                       return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
+                   }
+                   );
 
     server_->route("/device", QHttpServerRequest::Method::Put,
-                     [&controller](const QHttpServerRequest &request) {
-                         controller.start();
-                         return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
-                     }
-                     );
+                   [&controller](const QHttpServerRequest &request) {
+                       controller.start();
+                       return QHttpServerResponse(QString::fromStdString(controller.readResponse()));
+                   }
+                   );
 
     const auto port = server_->listen(QHostAddress::Any);
 
--- src/http-server/include/server.hpp
+++ src/http-server/include/server.hpp
@@ -9,6 +9,7 @@ class Server
 public:
     Server(DeviceController& controller);
     ~Server();
+
 private:
     DeviceController& controller_;
     std::unique_ptr<QHttpServer> server_;
