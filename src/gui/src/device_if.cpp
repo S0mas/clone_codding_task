5cf600f6a1ca2fdc48396f2ea4d47baa72de421e
krzysztof.sommerfeld <sommerfeld.krzysztof@gmail.com>
2024-04-10 17:00:42 +0200

    Add methods to read device state and measurement by gui

--- src/gui/src/device_if.cpp
+++ src/gui/src/device_if.cpp
@@ -5,6 +5,59 @@ DeviceIF::DeviceIF(DeviceController& controller, QObject* parent)
     , controller_{controller}
 {}
 
+auto DeviceIF::lastResponse() const -> QString
+{
+    return lastResponse_;
+}
+
+auto DeviceIF::setLastResponse(const QString &response) -> void
+{
+    lastResponse_ = response;
+    emit lastResponseChanged();
+}
+
+auto DeviceIF::pressure() const -> double
+{
+    return lastMeasurement_.pressure_;
+}
+
+auto DeviceIF::temperature() const -> double
+{
+    return lastMeasurement_.temperature_;
+}
+
+auto DeviceIF::velocity() const -> double
+{
+    return lastMeasurement_.velocity_;
+}
+
+auto DeviceIF::setMeasurement(const Measurement& m) -> void
+{
+    if(m != lastMeasurement_)
+    {
+        lastMeasurement_ = m;
+        emit lastMeasurementChanged();
+    }
+}
+
+auto DeviceIF::frequency() const -> int
+{
+    return config_.frequency_;
+}
+
+auto DeviceIF::debug() const -> bool
+{
+    return config_.debug_;
+}
+
+auto DeviceIF::setConfig(const Configuration& c) -> void
+{
+    if(c != config_)
+    {
+        config_ = c;
+        emit configChanged();
+    }
+}
 
 auto DeviceIF::start() -> void
 {
--- src/gui/src/device_if.hpp
+++ src/gui/src/device_if.hpp
@@ -1,12 +1,22 @@
 #pragma once
 
+#include <configuration.hpp>
 #include <device_controller.hpp>
+#include <measurement.hpp>
+#include <parser.hpp>
 
 #include <QObject>
 
 class DeviceIF : public QObject
 {
     Q_OBJECT
+
+    Q_PROPERTY(QString lastResponse READ lastResponse NOTIFY lastResponseChanged)
+    Q_PROPERTY(double pressure READ pressure NOTIFY lastMeasurementChanged)
+    Q_PROPERTY(double temperature READ temperature NOTIFY lastMeasurementChanged)
+    Q_PROPERTY(double velocity READ velocity NOTIFY lastMeasurementChanged)
+    Q_PROPERTY(int frequency READ frequency NOTIFY configChanged)
+    Q_PROPERTY(bool debug READ debug NOTIFY configChanged)
 
 public:
     DeviceIF(DeviceController& controller, QObject* parent = nullptr);
@@ -15,6 +25,25 @@ public:
     Q_INVOKABLE void stop();
     Q_INVOKABLE void configure(int frequency, bool debug);
 
+    auto lastResponse() const -> QString;
+    auto setLastResponse(const QString &response) -> void;
+    auto pressure() const -> double;
+    auto temperature() const -> double;
+    auto velocity() const -> double;
+    auto setMeasurement(const Measurement& m) -> void;
+    auto frequency() const -> int;
+    auto debug() const -> bool;
+    auto setConfig(const Configuration& c) -> void;
+
+public:
+signals:
+    void lastResponseChanged();
+    void lastMeasurementChanged();
+    void configChanged();
+
 private:
     DeviceController& controller_;
+    QString lastResponse_;
+    Measurement lastMeasurement_;
+    Configuration config_;
 };
